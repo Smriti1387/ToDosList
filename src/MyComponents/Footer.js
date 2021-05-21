b8da0684b5b78c9d14e9f1118f5f8f88f9def840
Smriti <54507520+Smriti1387@users.noreply.github.com>
2021-05-21 12:53:15 +0530

    Add files via upload

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+
+
+    return (
+        <>
+        <p className="bg-dark text-light text-center py-3">Copyright &copy; MyTodosList.com</p>
+        </>
+    )
+}
